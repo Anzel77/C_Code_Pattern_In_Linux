9084e570c52abfd8b0ff267580e10028c8bb79d5
Karl <karlmfloating@gmail.com>
2023-07-19 11:08:37 +0800

    Modify common

--- src/common/common.c
+++ src/common/common.c
@@ -1,29 +1,44 @@
 #include "common.h"
 
 #include <stdio.h>
+#include <stdint.h>
 
-void PutByteStream(uint8_t* stream, uint64_t srcValue, size_t numBytes, uint32_t* offset) {
+uint8_t* PutByteStream(uint8_t* stream, uint64_t srcValue, size_t numBytes, uint32_t* offset) {
     for (int i = numBytes - 1; i >= 0; i--) {
         *(stream + *offset) = (uint8_t)(srcValue >> (8 * i));
         (*offset) += 1;
     }
+
+    return stream;
 }
 
-void GetByteStream(uint8_t* stream, uint64_t* const dstValue, size_t numBytes, uint32_t* offset) {
-    for (int i = numBytes - 1; i >=0; i--) {
-        *dstValue |= *(stream + *offset) << (8 * i);
+uint64_t GetByteStream(uint8_t* stream, size_t numBytes, uint32_t* offset) {
+    uint64_t result = 0;
+
+    if (stream == NULL || numBytes > 8) return -1;
+
+    for (size_t i = 0; i < numBytes; i++) {
+        result = (result << 8) | stream[*offset];
         (*offset) += 1;
     }
+
+    return result;
 }
 
-void SaveInBigEndian(uint8_t* array, uint64_t value, size_t numBytes) {
+uint8_t* SaveInBigEndian(uint8_t* array, uint64_t value, size_t numBytes) {
     for (int i = 0; i < numBytes; i++) {
         array[numBytes - 1 - i] = (value >> (8 * i)) & 0xFF;
     }
+
+    return array;
 }
 
-void ExtractFromBigEndian(uint8_t* array, uint64_t* value, size_t numBytes) {
-    for (int i = 0; i < numBytes; i++) {
-        *value |= (array[i] << (numBytes - 1 - i)) & 0xFF;
+uint64_t ExtractFromBigEndian(uint8_t* array, size_t numBytes) {
+    uint64_t result = 0;
+
+    for (size_t i = 0; i < numBytes; i++) {
+        result = (result << 8) | array[i];
     }
+
+    return result;
 }
--- src/common/common.h
+++ src/common/common.h
@@ -10,43 +10,13 @@
 extern "C" {
 #endif
 
-/**
- * @brief Put data into a byte stream in big-endian form
- * 
- * @param stream [out] The pointer to stream where the value will be put into
- * @param srcValue [in] The value will be put into stream
- * @param numBytes [in] The number of bytes that need to be placed
- * @param offset [out] offset
- */
-void PutByteStream(uint8_t* stream, uint64_t srcValue, size_t numBytes, uint32_t* offset);
+uint8_t* PutByteStream(uint8_t* stream, uint64_t srcValue, size_t numBytes, uint32_t* offset);
 
-/**
- * @brief Get value from a big-endian byte stream
- * 
- * @param stream [in] The pointer to stream where the value will be got out of
- * @param dstValue [out] The pointer to saving value
- * @param numBytes [in] The number of bytes that need to be got
- * @param offset [out] offset
- */
-void GetByteStream(uint8_t* stream, uint64_t* const dstValue, size_t numBytes, uint32_t* offset);
+uint64_t GetByteStream(uint8_t* stream, size_t numBytes, uint32_t* offset);
 
-/**
- * @brief Store values ​​in big-endian format into an array
- * 
- * @param array [out] 
- * @param value [in] 
- * @param numBytes [in] 
- */
-void SaveInBigEndian(uint8_t* array, uint64_t value, size_t numBytes);
+uint8_t* SaveInBigEndian(uint8_t* array, uint64_t value, size_t numBytes);
 
-/**
- * @brief Extract numeric values from arrays stored in big-endian form
- * 
- * @param array [in] 
- * @param value [out] 
- * @param numBytes [in] 
- */
-void ExtractFromBigEndian(uint8_t* array, uint64_t* value, size_t numBytes);
+uint64_t ExtractFromBigEndian(uint8_t* array, size_t numBytes);
 
 
 
