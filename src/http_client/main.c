ad04d764dc50be2691ca955674bb7a4debab85c3
Karl <karlmfloating@gmail.com>
2023-11-29 10:27:47 +0800

    Update http_client

--- src/http_client/main.c
+++ src/http_client/main.c
@@ -3,27 +3,38 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/select.h>
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE (1024 * 500)
 
 int main(int argc, char const* argv[]) {
+    int                ret = 0;
     int                client_socket;
     struct sockaddr_in server_address;
-    char               server_ip[] = "10.0.0.100"; // 目标服务器的IP地址
+    char               server_ip[] = "10.0.0.200"; // 目标服务器的IP地址
     int                server_port = 80;           // 目标服务器的端口号
 
-    const char request_home[128]      = "GET /home HTTP/1.1\r\n"
-                                        "Host: example.com\r\n"
-                                        "Connection: close\r\n"
-                                        "\r\n";
-    const char request_cover_on[128]  = "GET /set_params?cover=1 HTTP/1.1\r\n"
-                                        "Host: example.com\r\n"
-                                        "Connection: close\r\n"
-                                        "\r\n";
+    // const char request_home[128] = "GET /home HTTP/1.1\r\n"
+    //                                "Host: example.com\r\n"
+    //                                "Connection: close\r\n"
+    //                                "\r\n";
+
+    const char request_cover_on[128] = "GET /set_params?cover=1 HTTP/1.1\r\n"
+                                       "Host: example.com\r\n"
+                                       "Connection: close\r\n"
+                                       "\r\n";
+
     const char request_cover_off[128] = "GET /set_params?cover=0 HTTP/1.1\r\n"
                                         "Host: example.com\r\n"
                                         "Connection: close\r\n"
                                         "\r\n";
+
+    const char request_get_snap[128] = "GET /1.jpg HTTP/1.1\r\n"
+                                       "Host: example.com\r\n"
+                                       "Connection: close\r\n"
+                                       "\r\n";
 
     int cnt = 0;
 
@@ -33,7 +44,7 @@ int main(int argc, char const* argv[]) {
         if (client_socket == -1) {
             perror("Failed to create socket");
             // exit(EXIT_FAILURE);
-            sleep(5);
+            sleep(1);
             continue;
         }
 
@@ -44,7 +55,7 @@ int main(int argc, char const* argv[]) {
             perror("Invalid address/ Address not supported");
             // exit(EXIT_FAILURE);
             close(client_socket);
-            sleep(5);
+            sleep(1);
             continue;
         }
 
@@ -53,7 +64,7 @@ int main(int argc, char const* argv[]) {
             perror("Connection failed");
             // exit(EXIT_FAILURE);
             close(client_socket);
-            sleep(5);
+            sleep(1);
             continue;
         }
 
@@ -68,11 +79,11 @@ int main(int argc, char const* argv[]) {
         //         argv[1]);
 
         if (cnt % 3 == 0) {
-            sprintf(request, request_home);
+            sprintf(request, request_get_snap);
         } else if (cnt % 3 == 1) {
-            sprintf(request, request_cover_on);
-        } else if (cnt % 3 == 2) {
             sprintf(request, request_cover_off);
+        } else if (cnt % 3 == 2) {
+            sprintf(request, request_cover_on);
         }
 
         // sprintf(request, request_home);
@@ -80,25 +91,62 @@ int main(int argc, char const* argv[]) {
         // 发送请求
         if (send(client_socket, request, strlen(request), 0) < 0) {
             perror("Failed to send request");
+            close(client_socket);
             exit(EXIT_FAILURE);
         }
 
         cnt++;
         printf("count: %d\n", cnt);
 
-        // 接收响应并输出
-        char response_buffer[BUFFER_SIZE];
-        int  received_bytes;
-        while ((received_bytes = recv(client_socket, response_buffer, BUFFER_SIZE - 1, 0)) > 0) {
-            response_buffer[received_bytes] = '\0';
-            printf("%s\n", response_buffer);
-            printf("====================================\n\n");
+        fd_set readset;
+        FD_ZERO(&readset);
+        FD_SET(client_socket, &readset);
+
+        struct timeval timeout;
+        timeout.tv_sec  = 1;
+        timeout.tv_usec = 0;
+
+        ret = select(client_socket + 1, &readset, NULL, NULL, &timeout);
+        if (ret == -1) {
+            //除了被信号中断的情形，其他情况都是出错
+            if (errno != EINTR) {
+                // close(client_socket);
+                // break;
+            }
+        } else if (ret == 0) {
+            // select函数超时
+            printf("select timeout\n");
+            // close(client_socket);
+            // break;
+        } else {
+            if (FD_ISSET(client_socket, &readset)) {
+                // 接收响应并输出
+                char response_buffer[BUFFER_SIZE];
+                int  received_bytes;
+                while ((received_bytes = recv(client_socket, response_buffer, BUFFER_SIZE - 1, 0)) > 0) {
+                    response_buffer[received_bytes] = '\0';
+                    printf("%s\n", response_buffer);
+                    printf("====================================\n\n");
+                }
+
+                if (received_bytes < 0) {
+                    //除了被信号中断的情形，其他情况都是出错
+                    if (errno != EINTR) {
+                        perror("recv");
+                        // close(client_socket);
+                        // break;
+                    }
+                } else if (received_bytes == 0) {
+                    // printf("对端关闭了连接\n");
+                }
+            }
         }
 
         // 关闭套接字
         close(client_socket);
 
-        sleep(1);
+        // sleep(1);
+        usleep(1000 * 500);
     }
 
     return 0;
